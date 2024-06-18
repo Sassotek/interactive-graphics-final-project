2034ac37366c5a21a6edd53d782452cc1536dbd1
Sassotek <99842937+Sassotek@users.noreply.github.com>
2024-06-18 18:55:49 +0200

    update

--- objects_drawer.js
+++ objects_drawer.js
@@ -44,9 +44,159 @@ function load_url(filePath)
     xmlhttp.send();
     if (xmlhttp.status==200) {
       result = xmlhttp.responseText;
-      console.log(result);
+      //console.log(result);
     }
     return result;
+}
+
+
+function obj_loader(obj_data)
+{
+
+    var ver_pos = [];
+    var faces = [];
+    var tex_pos = [];
+    var t_faces = [];
+    var norm_pos = [];
+    var n_faces = [];
+
+    var lines = obj_data.split('\n');
+    for ( var i=0; i<lines.length; ++i ) 
+        {
+            var line = lines[i].trim();
+            var elem = line.split(/\s+/);
+            switch ( elem[0][0] ) {
+                case 'v':
+                    switch ( elem[0].length ) 
+                    {
+                        case 1:
+                            ver_pos.push( [ parseFloat(elem[1]), parseFloat(elem[2]), parseFloat(elem[3]) ] );
+                            break;
+                        case 2:
+                            switch ( elem[0][1] ) 
+                            {
+                                case 't':
+                                    tex_pos.push( [ parseFloat(elem[1]), parseFloat(elem[2]) ] );
+                                    break;
+                                case 'n':
+                                    norm_pos.push( [ parseFloat(elem[1]), parseFloat(elem[2]), parseFloat(elem[3]) ] );
+                                    break;
+                            }
+                            break;
+                    }
+                break;
+                
+                case 'f':
+                    var f=[], tf=[], nf=[];
+                    for ( var j=1; j<elem.length; ++j ) 
+                        {
+                            var ids = elem[j].split('/');
+                            var vid = parseInt(ids[0]);
+                            if ( vid < 0 ) vid = ver_pos.length + vid + 1;
+                            f.push( vid - 1 );
+                            if ( ids.length > 1 && ids[1] !== "" ) 
+                                {
+                                    var tid = parseInt(ids[1]);
+                                    if ( tid < 0 ) tid = tex_pos.length + tid + 1;
+                                    tf.push( tid - 1 );
+                                }
+                            if ( ids.length > 2 && ids[2] !== "" ) 
+                                {
+                                    var nid = parseInt(ids[2]);
+                                    if ( nid < 0 ) nid = norm_pos.length + nid + 1;
+                                    nf.push( nid - 1 );
+                                }
+                        }
+                faces.push(f);
+                if ( tf.length ) t_faces.push(tf);
+                if ( nf.length ) n_faces.push(nf);
+                break;
+        }
+    }
+
+    var vertexBuffer = [];
+    var textureBuffer = [];
+    var normalBuffer = [];
+
+    for (var i = 0; i < faces.length; ++i) 
+        {
+            if(faces[i].length < 3) continue;
+
+            var f = faces[i];
+            var tf = t_faces[i];
+            var nf = n_faces[i];
+
+            vertexBuffer.push(ver_pos[f[0]][0]);
+            vertexBuffer.push(ver_pos[f[0]][1]);
+            vertexBuffer.push(ver_pos[f[0]][2]);
+            vertexBuffer.push(ver_pos[f[1]][0]);
+            vertexBuffer.push(ver_pos[f[1]][1]);
+            vertexBuffer.push(ver_pos[f[1]][2]);
+            vertexBuffer.push(ver_pos[f[2]][0]);
+            vertexBuffer.push(ver_pos[f[2]][1]);
+            vertexBuffer.push(ver_pos[f[2]][2]);
+            
+            if(tf)
+                {
+                    textureBuffer.push(tex_pos[tf[0]][0]);
+                    textureBuffer.push(tex_pos[tf[0]][1]);
+                    textureBuffer.push(tex_pos[tf[1]][0]);
+                    textureBuffer.push(tex_pos[tf[1]][1]);
+                    textureBuffer.push(tex_pos[tf[2]][0]);
+                    textureBuffer.push(tex_pos[tf[2]][1]);
+                }
+
+            if(nf)
+                {
+                    normalBuffer.push(norm_pos[f[0]][0]);
+                    normalBuffer.push(norm_pos[f[0]][1]);
+                    normalBuffer.push(norm_pos[f[0]][2]);
+                    normalBuffer.push(norm_pos[f[1]][0]);
+                    normalBuffer.push(norm_pos[f[1]][1]);
+                    normalBuffer.push(norm_pos[f[1]][2]);
+                    normalBuffer.push(norm_pos[f[2]][0]);
+                    normalBuffer.push(norm_pos[f[2]][1]);
+                    normalBuffer.push(norm_pos[f[2]][2]);
+                }
+            
+            for(var j = 3; j<faces[i]; ++j)
+                {
+                    vertexBuffer.push(ver_pos[f[0]][0]);
+                    vertexBuffer.push(ver_pos[f[0]][1]);
+                    vertexBuffer.push(ver_pos[f[0]][2]);
+                    vertexBuffer.push(ver_pos[f[j-1]][0]);
+                    vertexBuffer.push(ver_pos[f[j-1]][1]);
+                    vertexBuffer.push(ver_pos[f[j-1]][2]);
+                    vertexBuffer.push(ver_pos[f[j]][0]);
+                    vertexBuffer.push(ver_pos[f[j]][1]);
+                    vertexBuffer.push(ver_pos[f[j]][2]);
+                    
+                    if(tf)
+                        {
+                            textureBuffer.push(tex_pos[tf[0]][0]);
+                            textureBuffer.push(tex_pos[tf[0]][1]);
+                            textureBuffer.push(tex_pos[tf[j-1]][0]);
+                            textureBuffer.push(tex_pos[tf[j-1]][1]);
+                            textureBuffer.push(tex_pos[tf[j]][0]);
+                            textureBuffer.push(tex_pos[tf[j]][1]);
+                        }
+
+                    if(nf)
+                        {
+                            normalBuffer.push(norm_pos[f[0]][0]);
+                            normalBuffer.push(norm_pos[f[0]][1]);
+                            normalBuffer.push(norm_pos[f[0]][2]);
+                            normalBuffer.push(norm_pos[f[j-1]][0]);
+                            normalBuffer.push(norm_pos[f[j-1]][1]);
+                            normalBuffer.push(norm_pos[f[j-1]][2]);
+                            normalBuffer.push(norm_pos[f[j]][0]);
+                            normalBuffer.push(norm_pos[f[j]][1]);
+                            normalBuffer.push(norm_pos[f[j]][2]);
+                        }
+                }
+        }
+
+    return {vertex_buffer: vertexBuffer, texture_buffer: textureBuffer, normal_buffer: normalBuffer};
 }
 
 
@@ -226,9 +376,13 @@ class quaoar_drawer
     constructor()
     {
         this.obj_file = load_url("http://0.0.0.0:8000/Quaoar.obj");
-        this.tex_img = load_url("http://0.0.0.0:8000/quaoar_texture.png");
+        this.tex_file = load_url("http://0.0.0.0:8000/quaoar_texture.png");
+
+        tex_img = new Image();
+        var imageURL = window.URL.createObjectURL();
+
         
-        var VertexShaderText = `
+        this.VertexShaderText = `
                 precision mediump float;
 
                 uniform mat4 mvp;
@@ -241,11 +395,11 @@ class quaoar_drawer
                 void main()
                 {
                     gl_Position = mvp*vec4(pos,1);
-                    tex_coord = v_tex_coord;
+                    v_tex_coord = tex_coord;
                 }
             `;
 
-        var FragmentShaderText = `
+        this.FragmentShaderText = `
                 precision mediump float;
 
                 uniform sampler2D sampler;
@@ -254,10 +408,46 @@ class quaoar_drawer
 
                 void main()
                 {
-                    gl_FragColor(sampler, v_tex_coord);
+                    gl_FragColor = texture2D(sampler, v_tex_coord);
                 }
             `;
+        
+        this.obj_data = obj_loader(this.obj_file);
+        
+        this.vertices = this.obj_data.vertex_buffer;
+        this.texture_c = this.obj_data.texture_buffer;
+        this.normals = this.obj_data.normal_buffer;
+
+        this.prog = program_init(this.VertexShaderText, this.FragmentShaderText);
+        gl.useProgram(this.prog);
+
+        this.sampler = gl.getUniformLocation(this.prog, 'sampler');
+
+        this.vertexBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vertices), gl.STATIC_DRAW);
+
+        this.textureBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.textureBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.texture_c), gl.STATIC_DRAW);
+
+        this.texture = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D, this.texture);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, this.tex_img);
+        gl.generateMipmap(gl.TEXTURE_2D);
+        gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT );
+		gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT );
+		gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR );
+		gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR );
+        gl.uniform1i(this.sampler, 0);
+
+        this.normalsBuffer = gl.createBuffer();
+
+        
     }
+
+
+    
 
     draw(m_v)
     {
